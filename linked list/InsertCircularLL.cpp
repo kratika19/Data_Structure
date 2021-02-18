755b690cd5630f9e5d76b5ce378f6d814ba1fdfa
KRATIKA MITTAL <56781757+kratika19@users.noreply.github.com>
2021-02-18 13:11:14 +0530

    linked list

--- linked list/InsertCircularLL.cpp
+++ linked list/InsertCircularLL.cpp
@@ -23,6 +23,26 @@ void createCLL(int A[], int n) {
     }
 }
 
+void insert(int pos, int x) {
+    struct Node *t, *p;
+    t = new Node;
+    t->next = nullptr;
+    t->data = x;
+    p = head;
+    if (pos == 0) {
+        while (p->next != head)
+            p = p->next;
+        t->next = head;
+        p->next = t;
+        head = t;
+    } else {
+        for (int i = 0; i < pos - 1; i++)
+            p = p->next;
+        t->next = p->next;
+        p->next = t;
+    }
+}
+
 void display(struct Node *p) {
     int flag = 0;
     while (flag != 1 || p != head) {
@@ -33,34 +53,15 @@ void display(struct Node *p) {
 }
 
 
-bool isLoop(struct Node *f) {
-    bool check = false;
-    struct Node *p, *q;
-    p = q = f;
-    do {
-        p = p->next;
-        q = q->next;
-        if (q != NULL)
-            q = q->next;
-        if (p == q) {
-            check = true;
-            break;
-        }
-    } while (p != NULL && q != NULL);
-    return check;
-}
-
-
 int main() {
     int A[] = {1, 2, 3, 4};
     createCLL(A, 4);
     display(head);
     cout << endl;
-    bool check = isLoop(head);
-    if (check)
-        cout << "Loop is present in the Linked List" << endl;
-    else
-        cout << "Loop is absent in the linked list" << endl;
+    int pos = 5;//position of insertion
+    int x = 8;//value to be inserted
+    insert(pos-1, x);
+    display(head);
     cout << endl;
     return 0;
 }
