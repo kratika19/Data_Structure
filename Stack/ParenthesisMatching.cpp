6486616346f3f557ec3e7f634f96a13a3950b1d0
KRATIKA MITTAL <56781757+kratika19@users.noreply.github.com>
2021-02-27 15:50:17 +0530

    Parenthesis Matched

--- Stack/ParenthesisMatching.cpp
+++ Stack/ParenthesisMatching.cpp
@@ -1,4 +1,56 @@
-//
-// Created by krati on 27-02-2021.
-//
+#include <iostream>
+#include <cstring>
+using namespace std;
 
+struct Stack{
+    int size;
+    int top;
+    char *Ar;
+}stack;
+
+void push(char c)
+{
+    stack.Ar[stack.top] = c;
+    stack.top++;
+}
+
+void pop()
+{
+    stack.top--;
+}
+
+bool checkParenthesis(char *p)
+{
+    stack.size = strlen(p);
+    stack.top = -1;
+    stack.Ar = new char [stack.size];
+    for(int i=0;i<stack.size;i++)
+        if(p[i] == '(')
+            push(p[i]);
+        else if(p[i]==')')
+            pop();
+        else
+            continue;
+
+    if(stack.top == -1)
+        return true;
+    else
+        return false;
+}
+int main()
+{
+    string eq;
+    cout<<"Enter the equation :";
+    getline(cin,eq);
+    cout<<eq<<endl;
+    int l = eq.length() + 1;
+    char ch[l];
+    strcpy(ch, eq.c_str());
+    bool ab = checkParenthesis(ch);
+    if(ab)
+        cout<<"Parenthesis Matched"<<endl;
+    else
+        cout<<"Parenthesis not Matched"<<endl;
+
+    return 0;
+}
