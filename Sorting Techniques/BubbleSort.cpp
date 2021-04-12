fea3d197691ad1a54a7b20a974f0b7a197894246
KRATIKA MITTAL <56781757+kratika19@users.noreply.github.com>
2021-04-12 15:46:24 +0530

    bubble sort

--- Sorting Techniques/BubbleSort.cpp
+++ Sorting Techniques/BubbleSort.cpp
@@ -3,38 +3,41 @@
 //
 
 #include <iostream>
+
 using namespace std;
 
-void bubbleSort(int A[], int n)
-{
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<n-i-1;j++)
-        {
-            if(A[j]>A[j+1])
-            {
+void bubbleSort(int A[], int n) {
+    int flag, pass;
+    pass = 0;
+    for (int i = 0; i < n - 1; i++) {
+        flag = 0;
+        pass++;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (A[j] > A[j + 1]) {
                 int temp = A[j];
-                A[j] = A[j+1];
-                A[j+1] = temp;
+                A[j] = A[j + 1];
+                A[j + 1] = temp;
+                flag = 1;
             }
         }
+        if (flag == 0)
+            break;
     }
-    cout<<"Elements after sorting"<<endl;
-    for(int i = 0;i<n;i++)
-    {
-        cout<<A[i]<<" ";
+    cout << "Elements after sorting" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << A[i] << " ";
     }
+    cout<<endl<<endl<<"No of passes = "<<pass<<endl;
 }
 
-int main()
-{
-    int A[] = {91,22,89,11,2,65,41,30,2};
-    cout<<"Elements before sorting"<<endl;
-    for(int i = 0;i<9;i++)
-    {
-        cout<<A[i]<<" ";
+int main() {
+    int A[] = {91, 22, 89, 11, 2, 65, 41, 30, 2};
+//    int A[] = {1,2,3,4,5,6,7,8,9};
+    cout << "Elements before sorting" << endl;
+    for (int i = 0; i < 9; i++) {
+        cout << A[i] << " ";
     }
-    cout<<endl;
-    bubbleSort(A,9);
+    cout << endl<<endl;
+    bubbleSort(A, 9);
     return 0;
 }
