a3ea45023836f55ba4ae8ea65b75eed77c214433
KRATIKA MITTAL <56781757+kratika19@users.noreply.github.com>
2021-04-13 17:14:18 +0530

    merge array

--- Array/merge Array.cpp
+++ Array/merge Array.cpp
@@ -2,22 +2,17 @@
 
 using namespace std;
 
-
-void merge(int A[], int m, int B[], int n, int* C)
-{
-    int s = m+n;
+//my algo
+void merge(int A[], int m, int B[], int n, int *C) {
+    int s = m + n;
     int a = 0;
     int b = 0;
-    for(int i=0;i<s;i++)
-    {
-        if(a<=m && b<=n)
-        {
-            if(A[a]<B[b])
-            {
+    for (int i = 0; i < s; i++) {
+        if (a <= m && b <= n) {
+            if (A[a] < B[b]) {
                 C[i] = A[a];
                 a++;
-            }
-            else{
+            } else {
                 C[i] = B[b];
                 b++;
             }
@@ -25,32 +20,56 @@ void merge(int A[], int m, int B[], int n, int* C)
     }
 }
 
-int main()
-{
+//new algo
+void mergeArray(int A[], int m, int B[], int n, int *C) {
+    int i, j, k;
+    i = j = k = 0;
+    while (i <= m && j <= n) {
+        if (A[i] < B[j])
+            C[k++] = A[i++];
+        else
+            C[k++] = B[j++];
+    }
+    for (; i < m; i++)
+        C[k++] = A[i++];
+    for (; j < n; j++)
+        C[k++] = B[j++];
+}
+
+int main() {
     int m, n;
-    cout<<"Enter size of Array 1 :";
-    cin>>m;
-    cout<<"Enter the elements of Array 1"<<endl;
+    cout << "Enter size of Array 1 :";
+    cin >> m;
+    cout << "Enter the elements of Array 1" << endl;
     int A[m];
     for (int i = 0; i < m; ++i) {
-        cin>>A[i];
+        cin >> A[i];
     }
-    cout<<endl;
-    cout<<"Enter size of Array 2 :";
-    cin>>n;
-    cout<<"Enter the elements of Array 2"<<endl;
+    cout << endl;
+    cout << "Enter size of Array 2 :";
+    cin >> n;
+    cout << "Enter the elements of Array 2" << endl;
     int B[n];
     for (int i = 0; i < n; ++i) {
-        cin>>B[i];
+        cin >> B[i];
     }
-    int* C = new int [m+n];
+    int *C = new int[m + n];
 
-    merge(A,m,B,n,C);
+    merge(A, m, B, n, C);
 
-    cout<<"Elements of new merged array are"<<endl;
-    for (int i = 0; i < m+n; ++i) {
-        cout<<C[i]<<" ";
+    cout << "Elements of new merged array are" << endl;
+    for (int i = 0; i < m + n; ++i) {
+        cout << C[i] << " ";
     }
-
     cout<<endl;
+
+    int *Ce = new int[m + n];
+    mergeArray(A, m, B, n, Ce);
+
+    cout << "Elements of new merged array are" << endl;
+    for (int i = 0; i < m + n; ++i) {
+        cout << Ce[i] << " ";
+    }
+
+    cout << endl;
 }
