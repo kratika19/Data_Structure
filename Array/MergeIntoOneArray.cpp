0a753ccfbe1db391cf1e777b0c49c10fd03f1f8f
KRATIKA MITTAL <56781757+kratika19@users.noreply.github.com>
2021-04-16 02:15:16 +0530

    merge array into one

---
+++ Array/MergeIntoOneArray.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+//new algo
+void mergeArray(int A[], int l, int h, int m, int C[]) {
+    int i, j, k;
+    i = l;
+    j = m;
+    k = l;
+    while (i <= m && j <= h) {
+        if (A[i] < A[j])
+            C[k++] = A[i++];
+        else
+            C[k++] = A[j++];
+    }
+    for (; i < m; i++)
+        C[k++] = A[i++];
+    for (; j < h; j++)
+        C[k++] = A[j++];
+}
+
+int main() {
+    int m;
+    cout << "Enter size of Array :";
+    cin >> m;
+    cout << "Enter the elements of Array " << endl;
+    int A[m];
+    for (int i = 0; i < m; ++i) {
+        cin >> A[i];
+    }
+    cout << endl;
+    int C[m];
+    mergeArray(A, 0, m, m / 2, C);
+    cout << "Elements of new merged array are" << endl;
+    for (int i = 0; i < m; ++i) {
+        cout << C[i] << " ";
+    }
+    cout << endl;
+}
