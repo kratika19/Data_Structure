fe5925230ab0bdb996d656028423be9323c79578
KRATIKA MITTAL <56781757+kratika19@users.noreply.github.com>
2021-04-16 15:53:39 +0530

    merge sort iterative

--- Sorting Techniques/Sort.cpp
+++ Sorting Techniques/Sort.cpp
@@ -88,7 +88,7 @@ void insertionSort(int A[], int n) {
 
 void selectionSort(int A[], int n) {
     int i, j, k;
-    for (int i = 0; i < n - 1; i++) {
+    for (i = 0; i < n - 1; i++) {
         for (j = k = i; j < n; ++j) {
             if (A[j] < A[k])
                 k = j;
