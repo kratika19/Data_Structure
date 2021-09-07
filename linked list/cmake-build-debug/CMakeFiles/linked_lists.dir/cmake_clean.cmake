8d967ae2fad05eb50b73f2fb2ce8fb320cb37313
KRATIKA MITTAL <56781757+kratika19@users.noreply.github.com>
2021-09-07 13:34:21 +0530

    knapsack problem

--- linked list/cmake-build-debug/CMakeFiles/linked_lists.dir/cmake_clean.cmake
+++ linked list/cmake-build-debug/CMakeFiles/linked_lists.dir/cmake_clean.cmake
@@ -1,5 +1,5 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/linked_lists.dir/G_/Semester4/dsa/Sorting_Techniques/QuickSort.cpp.obj"
+  "CMakeFiles/linked_lists.dir/G_/Semester4/dsa/Algorithm/greedy/Knapsack.cpp.obj"
   "liblinked_lists.dll.a"
   "linked_lists.exe"
   "linked_lists.exe.manifest"
